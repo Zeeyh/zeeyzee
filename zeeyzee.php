5192606fc9dd7c8095adfc54d1825bc4f6f76148
Zeeyh <36566387+Zeeyh@users.noreply.github.com>
2018-04-18 14:07:05 -1000

    Add files via upload

---
+++ zeeyzee.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Intern4</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head
+>
+<body>
+<div>
+<p><h3><?php echo "The time is" .date("h:i:sp");?></h3></p>
+	<h1>HNG INTERNSHIP-4</h1>
+	<p><h2>Hello World!</h2></p>
+</div>
+</body>
+</html>
